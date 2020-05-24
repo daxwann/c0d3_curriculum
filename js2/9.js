5c501dfa8b8fa07c23fd272470b66f327cd22765
Dax Wann <daxwann@gmail.com>
2020-05-24 16:23:20 -0700

    complete prob 9

--- js2/9.js
+++ js2/9.js
@@ -5,8 +5,10 @@
  */
 
 const solution = () => {
-  Array.prototype.cReduce = function () {
-    return 0
+  Array.prototype.cReduce = function (cb, accumulator = 0, i = 0) {
+    if (this[i] !== undefined) accumulator = cb(accumulator, this[i], i, this);
+    if (this[i + 1] !== undefined) return this.cReduce(cb, accumulator, i + 1);
+    return accumulator;
   }
 }
 
