f6cf43f403c22aada3f72b06211376bffca9db40
Dax Wann <daxwann@gmail.com>
2020-05-24 16:06:52 -0700

    complete prob 8

--- js2/8.js
+++ js2/8.js
@@ -5,8 +5,10 @@
  */
 
 const solution = () => {
-  Array.prototype.cMap = function (cb) {
-    return 0
+  Array.prototype.cMap = function (cb, newArr = [], i = 0) {
+    if (this[i] !== undefined) newArr.push(cb(this[i], i, this));
+    if (this[i + 1] !== undefined) return this.cMap(cb, newArr, i + 1);
+    return newArr;
   }
 }
 
