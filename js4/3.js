1a9a082468373bbdefb31c195263bdc741599a5f
Dax Wann <daxwann@gmail.com>
2020-07-25 12:53:56 -0700

    add mousemove event handler

--- js4/3.js
+++ js4/3.js
@@ -1,16 +1,34 @@
-const fairSmall = document.getElementById("fairSmall");
-const scope = document.createElement("div");
-scope.classList.add("scope");
-
-const appendScope = (element, scope) => e => {
+const createAppendScopeEventHandler = (element, scope, moveScopeEventHandler) => e => {
+  console.log('append scope')
   element.appendChild(scope);
-  const scopeRect = scope.getBoundingClientRect();
-  scope.setAttribute("style", `display: block; top: ${e.clientY - (scopeRect.height / 2)}px; left: ${e.clientX - (scopeRect.width / 2)}px`);
+  element.addEventListener("mousemove", moveScopeEventHandler);
+  setScopeCoordinates(scope, e);
 };
 
-const removeScope = (element, scope) => e => {
-  element.removeChild(scope);
+const createMoveScopeEventHandler = scope => e => {
+  setScopeCoordinates(scope, e);
 }
 
-fairSmall.addEventListener("mouseenter", appendScope(fairSmall, scope));
-fairSmall.addEventListener("mouseleave", removeScope(fairSmall, scope));
+const createRemoveScopeEventHandler = (element, scope, moveScopeEventHandler) => e => {
+  console.log('remove scope');
+  element.removeChild(scope);
+  element.removeEventListener("mousemove", moveScopeEventHandler);
+}
+
+function setScopeCoordinates(scope, event) {
+  console.log('set scope');
+  const scopeRect = scope.getBoundingClientRect();
+  scope.setAttribute("style", `display: block; top: ${event.clientY - (scopeRect.height / 2)}px; left: ${event.clientX - (scopeRect.width / 2)}px`);
+}
+
+function addEventListenersToPicture() {
+  const fairSmall = document.getElementById("fairSmall");
+  const scope = document.createElement("div");
+  scope.classList.add("scope");
+  const moveScopeEventHandler = createMoveScopeEventHandler(scope);
+  
+  fairSmall.addEventListener("mouseenter", createAppendScopeEventHandler(fairSmall, scope, moveScopeEventHandler));
+  fairSmall.addEventListener("mouseleave", createRemoveScopeEventHandler(fairSmall, scope, moveScopeEventHandler));
+}
+
+addEventListenersToPicture();
