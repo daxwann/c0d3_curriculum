ab83c25ccb3d264cccd1218a7dfa15eebfcb1704
Dax Wann <daxwann@gmail.com>
2020-07-25 23:46:19 -0700

    fix pointer event issue

--- js4/3.js
+++ js4/3.js
@@ -1,34 +1,24 @@
-const createAppendScopeEventHandler = (element, scope, moveScopeEventHandler) => e => {
-  console.log('append scope')
-  element.appendChild(scope);
-  element.addEventListener("mousemove", moveScopeEventHandler);
-  setScopeCoordinates(scope, e);
-};
-
 const createMoveScopeEventHandler = scope => e => {
   setScopeCoordinates(scope, e);
 }
 
-const createRemoveScopeEventHandler = (element, scope, moveScopeEventHandler) => e => {
-  console.log('remove scope');
-  element.removeChild(scope);
-  element.removeEventListener("mousemove", moveScopeEventHandler);
+const createRemoveScopeEventHandler = scope => e => {
+  scope.style.display = "none";
 }
 
 function setScopeCoordinates(scope, event) {
   console.log('set scope');
-  const scopeRect = scope.getBoundingClientRect();
-  scope.setAttribute("style", `display: block; top: ${event.clientY - (scopeRect.height / 2)}px; left: ${event.clientX - (scopeRect.width / 2)}px`);
+  scope.style.display = "block";
+  scope.style.top = `${event.clientY - (scope.offsetHeight / 2)}px`; 
+  scope.style.left = `${event.clientX - (scope.offsetWidth / 2)}px`;
 }
 
 function addEventListenersToPicture() {
   const fairSmall = document.getElementById("fairSmall");
-  const scope = document.createElement("div");
-  scope.classList.add("scope");
-  const moveScopeEventHandler = createMoveScopeEventHandler(scope);
+  const scope = document.getElementById("scope");
   
-  fairSmall.addEventListener("mouseenter", createAppendScopeEventHandler(fairSmall, scope, moveScopeEventHandler));
-  fairSmall.addEventListener("mouseleave", createRemoveScopeEventHandler(fairSmall, scope, moveScopeEventHandler));
+  fairSmall.addEventListener("mousemove", createMoveScopeEventHandler(scope));
+  fairSmall.addEventListener("mouseout", createRemoveScopeEventHandler(scope));
 }
 
 addEventListenersToPicture();
