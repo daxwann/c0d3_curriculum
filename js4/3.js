8ddf999cc18d7506f79a0bd575edda96f4500038
Dax Wann <daxwann@gmail.com>
2020-07-26 17:24:51 -0700

    move background position in move scope event handler

--- js4/3.js
+++ js4/3.js
@@ -1,24 +1,41 @@
-const createMoveScopeEventHandler = scope => e => {
-  setScopeCoordinates(scope, e);
-}
+const RATIO_X = 1263 / 421;
+const RATIO_Y = 503 / 168;
 
-const createRemoveScopeEventHandler = scope => e => {
-  scope.style.display = "none";
+const createMoveScopeEventHandler = (scope, smallFair) => (e) => {
+  setScopeCoordinates(scope, e);
+  moveScopeBackgroundPosition(e, scope, smallFair);
+};
+
+const createRemoveScopeEventHandler = (scope) => (e) => {
+  scope.style.display = 'none';
+};
+
+function moveScopeBackgroundPosition(event, scope, smallFair) {
+  const scopeInSmallFairTop = event.clientY - smallFair.offsetTop;
+  const scopeInSmallFairLeft = event.clientX - smallFair.offsetLeft;
+  scope.style.backgroundPositionY = `${
+    -1 * scopeInSmallFairTop * RATIO_Y + scope.offsetHeight / 2
+  }px`;
+  scope.style.backgroundPositionX = `${
+    -1 * scopeInSmallFairLeft * RATIO_X + scope.offsetWidth / 2
+  }px`;
 }
 
 function setScopeCoordinates(scope, event) {
-  console.log('set scope');
-  scope.style.display = "block";
-  scope.style.top = `${event.clientY - (scope.offsetHeight / 2)}px`; 
-  scope.style.left = `${event.clientX - (scope.offsetWidth / 2)}px`;
+  scope.style.display = 'block';
+  scope.style.top = `${event.clientY - scope.offsetHeight / 2}px`;
+  scope.style.left = `${event.clientX - scope.offsetWidth / 2}px`;
 }
 
 function addEventListenersToPicture() {
-  const fairSmall = document.getElementById("fairSmall");
-  const scope = document.getElementById("scope");
-  
-  fairSmall.addEventListener("mousemove", createMoveScopeEventHandler(scope));
-  fairSmall.addEventListener("mouseout", createRemoveScopeEventHandler(scope));
+  const fairSmall = document.getElementById('fairSmall');
+  const scope = document.getElementById('scope');
+
+  fairSmall.addEventListener(
+    'mousemove',
+    createMoveScopeEventHandler(scope, fairSmall)
+  );
+  fairSmall.addEventListener('mouseout', createRemoveScopeEventHandler(scope));
 }
 
 addEventListenersToPicture();
